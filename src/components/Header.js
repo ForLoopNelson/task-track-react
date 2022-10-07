119299bd2c2360fca52bda986db4cb3dc2ca6d09
Cory Nelson <fullnelsontech@gmail.com>
2022-10-07 15:50:32 -0400

    Second commit in progress

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,26 @@
-const Header = () => {
+import PropTypes from "prop-types"
+import Button from "./Button"
+
+const Header = ({ title }) => {
   return (
-    <header>
-      <h1>Task Track With React</h1>
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color="green" text="Add" />
     </header>
   )
 }
+
+Header.defaultProps = {
+  title: "Task Track With React",
+}
+
+Header.propTypes = {
+  title: PropTypes.string,
+}
+//You can style stuff inline with variables
+// const headingStyle = {
+//   color: "red",
+//   backgroundColor: "black",
+// }
 
 export default Header
